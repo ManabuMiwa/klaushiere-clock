9cbb25a7164435beb0f40f2d8f0db0c65691bb46
Manabu Miwa <manabumiwa_info@icloud.com>
2022-09-05 07:48:16 +0900

    Add transition on priod-changes

--- src/star-wheel.tsx
+++ src/star-wheel.tsx
@@ -5,8 +5,11 @@ import { ReactComponent as Inner } from "./imgs/inner.svg"
 type StarWheelProps = { time: KlaushierianTime }
 
 export default function StarWheel(props: StarWheelProps) {
+  const isDapre = props.time.period === 'dapre'
   const style: React.CSSProperties = {
     transform: `rotate(${props.time.innerAngle}deg)`,
+    filter: `grayscale(${isDapre ? '0%' : '75%'}) brightness(${isDapre ? 1 : .5})`,
+    transition: 'filter 5s'
   }
   return <Inner style={style} />
 }
--- src/sun-wheel.tsx
+++ src/sun-wheel.tsx
@@ -5,8 +5,11 @@ import { ReactComponent as Outer } from "./imgs/outer.svg"
 type SunWheelProps = { time: KlaushierianTime }
 
 export default function SunWheel(props: SunWheelProps) {
+  const isNotDapre = props.time.period !== 'dapre'
   const style: React.CSSProperties = {
     transform: `rotate(${props.time.outerAngle}deg)`,
+    filter: `grayscale(${isNotDapre ? '0%' : '75%'}) brightness(${isNotDapre ? 1 : .5})`,
+    transition: 'filter 5s'
   }
   return <Outer style={style} />
 }
