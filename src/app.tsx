c8a5c5bad224df93c1ecc2ffa97fa83a7eaee4ce
Manabu Miwa <manabumiwa_info@icloud.com>
2022-09-05 23:33:05 +0900

    Upgrade images

--- src/app.tsx
+++ src/app.tsx
@@ -4,6 +4,9 @@ import { KlaushierianTime } from './klaushierian-time'
 import SunWheel from './sun-wheel'
 import MoonWheel from './moon-wheel'
 import StarWheel from './star-wheel'
+import { ReactComponent as Sun } from './imgs/sun.svg'
+import { ReactComponent as Moon } from './imgs/moon.svg'
+import { ReactComponent as Star } from './imgs/star.svg'
 
 function App() {
   const [time, setTime] = useState(new KlaushierianTime())
@@ -29,6 +32,11 @@ function App() {
         </section>
         <section className="app--star-wheel">
           <StarWheel time={time} />
+        </section>
+        <section className="app--marks">
+          <Sun title="Sun" />
+          <Moon title="Moon" />
+          <Star title="Star" />
         </section>
       </section>
     </main>
